40dce03160d16d633d36f7cd660685ae9d75fe45
Melita Župarić <meli.zuparic@gmail.com>
2025-11-15 02:17:29 +0100

    Melita Župarić v03

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,91 @@
 #include "app.h"
 #include <algorithm>
 
-namespace vsite::oop::v3
+namespace vsite::oop::v3 
 {
+    results::results(int n) {
+        size = n;
+        count = 0;
+        arr = new student[n];
+    }
 
+    void results::add(const student& s) {
+        arr[count] = s;
+        count++;
+    }
+
+    unsigned results::has_grade(int g) const {
+        unsigned c = 0;
+        for (int i = 0; i < count; i++) {
+            if (arr[i].grade == g)
+                c++;
+        }
+        return c;
+    }
+
+    unsigned results::starts_with_letter(char c) const {
+        unsigned b = 0;
+        for (int i = 0; i < count; i++) {
+            if (tolower(arr[i].name[0]) == tolower(c))
+                b++;
+        }
+        return b;
+    }
+
+    array::array() {
+        m_size = 0;
+        m_data = nullptr;
+    }
+
+    array::array(unsigned size, double value) {
+        m_size = size;
+        m_data = new double[size];
+        for (int i = 0; i < size; i++) {
+            m_data[i] = value;
+        }
+    }
+
+    array::array(const array& other) {
+        m_size = other.m_size;
+        if (m_size == 0) {
+            m_data = nullptr;
+            return;
+        }
+        m_data = new double[m_size];
+        for (int i = 0; i < m_size; i++) {
+            m_data[i] = other.m_data[i];
+        }
+    }
+
+    array::array(array&& other) {
+        m_size = other.m_size;
+        m_data = other.m_data;
+
+        other.m_size = 0;
+        other.m_data = nullptr;
+    }
+
+    unsigned array::size() const {
+        return m_size;
+    }
+
+    double array::at(unsigned index) const {
+        if (index >= m_size)
+            return 0.;
+        return m_data[index];
+    }
+
+    void array::push_back(double value) {
+        double* temp = new double[m_size + 1];
+
+        for (int i = 0; i < m_size; i++) {
+            temp[i] = m_data[i];
+        }
+
+        temp[m_size] = value;
+
+        delete[] m_data;
+        m_data = temp;
+        m_size++;
+    }
 }
--- app/app.h
+++ app/app.h
@@ -4,4 +4,39 @@
 namespace vsite::oop::v3 
 {
 
+    struct student {
+        std::string name;
+        int grade;
+    };
+
+    class results {
+    public:
+        results(int n);
+        void add(const student& s);
+        unsigned has_grade(int g) const;
+        unsigned starts_with_letter(char c) const;
+
+    private:
+        student* arr;
+        int size;
+        int count;
+    };
+
+    class array {
+
+    public:
+        array();
+        array(unsigned size, double value);
+        array(const array& other);
+        array(array&& other);
+
+        unsigned size() const;
+        double at(unsigned index) const;
+        void push_back(double value);
+
+    private:
+        unsigned m_size;
+        double* m_data;
+    };
+
 }
